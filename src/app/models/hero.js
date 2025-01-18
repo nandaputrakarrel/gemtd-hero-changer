7acd94d83507735eedf526e5f752ae0019e41783
Nanda Putra Karrel R <nandaputrakarrel@gmail.com>
2025-01-18 15:31:19 +0700

    feat: small fixes

--- src/app/models/hero.js
+++ src/app/models/hero.js
@@ -1,5 +1,6 @@
 const heroes = require('../enums/heroes');
 const effects = require('../enums/effects');
+const potrait = require('../enums/potrait');
 
 const ability = require('./ability')
 
@@ -14,7 +15,7 @@ async function fromData(id, data) {
     hero: heroes[id],
     effect: data.effect ? effects[data.effect] : null,
     abilities: heroAbilities,
-    heroImage: `https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/${heroes[id].toLowerCase().replace(/\s/g, '')}.png`
+    heroImage: potrait[id]
   }
 }
 
