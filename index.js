c680e6055fbae45d6dc7eb47db3207752713b5a2
Nanda Putra Karrel R <nandaputrakarrel@gmail.com>
2025-01-18 17:37:24 +0700

    fix: cors

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(logger(JSON.stringify(logFormat)));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.json({limit: '1mb'}));
 app.use(express.urlencoded({limit: '1mb', extended: true}));
 app.use(express.static('public'));
