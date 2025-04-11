21371d6e24debce77439675d1e0f286c8d3858fe
Nanda Putra Karrel R <nandaputrakarrel@gmail.com>
2025-04-12 00:43:41 +0700

    feat: update vercel

--- index.js
+++ index.js
@@ -22,15 +22,15 @@ const logFormat = {
 
 app.use(logger(JSON.stringify(logFormat)));
 
-// const corsOptions = {
-//   origin: process.env.CORS_HOST.split(","),
-//   methods: ["GET,POST"],
-// };
+const corsOptions = {
+  origin: process.env.CORS_HOST.split(","),
+  methods: ["GET,POST"],
+};
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(express.json({limit: '1mb'}));
 app.use(express.urlencoded({limit: '1mb', extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -64,9 +64,9 @@ app.use((err, req, res, next) => {
 
 vercelSpeedInsights.injectSpeedInsights()
 
-const defaultPort = process.env.PORT || 3000;
-app.listen(defaultPort, () => {
-  console.log(`GemTD Updater started at port : ${defaultPort}`);
-});
+// const defaultPort = process.env.PORT || 3000;
+// app.listen(defaultPort, () => {
+//   console.log(`GemTD Updater started at port : ${defaultPort}`);
+// });
 
-// module.exports = app;
+module.exports = app;
